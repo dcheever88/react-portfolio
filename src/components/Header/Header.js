7eb82e7b33ac4a1bbd7cee387b01f7d4a05ca9e9
dcheever <cheeverdanny@gmail.com>
2021-07-16 19:42:26 -0500

    create header and footer components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Header Test</h1>
+            <p>test test test</p>
+        </div>
+    )
+}
+
+export default Header;
