f7b6f6bfa8915d24ad9b06cf1fa385a956159249
dcheever <cheeverdanny@gmail.com>
2021-09-13 20:09:47 -0500

    save again

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,25 +3,25 @@ import React from 'react';
 const Resume = () => {
     return (
         <section class="container">
-            <h2 className="title">Resume</h2>
+             <div>
+             <h2 className="title">Proficiencies</h2>
+                    <h3 class="d-flex justify-content-evenly">Full-Stack Web Developer</h3>
 
-            <div>
+                    <p class="d-flex justify-content-evenly mt-5">
+                        HTML, CSS (Bootstrap, HandleBars, Bulma), Javascript, React.js, jQuery <br />
+                        
+                        APIs, Node.js, Express.js, IndexedDB, SQL, NoSQL, Sequelize, Mongoose, MongoDB
+                    </p>
+            <h2 className="title">Resume</h2>
                 <div class="mt-5">
 
-                    <h3>
+                    <h3 class="d-flex justify-content-evenly">
                         <a href="https://github.com/dcheever88" class="link px-2">GitHub</a>
                     
                     
                         <a href="https://www.linkedin.com/in/danny-cheever/" class="link">LinkedIn</a>
                     
                     </h3>
-                    <h2 className="title">Proficiencies</h2>
-                    <h3>Full-Stack Web Developer</h3>
-
-                    <p class="mt-5">
-                        HTML, CSS (Bootstrap, HandleBars, Bulma), Javascript, React.js, jQuery, <br />
-                        APIs, Node.js, Express.js, IndexedDB, SQL, NoSQL, Sequelize, Mongoose, MongoDB
-                    </p>
                 </div>
             </div>
         </section>
